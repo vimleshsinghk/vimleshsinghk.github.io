b4d3f1171d1385862948c2ff7a53571ee607b9a2
vimlesh Singh <vimleshsinghk@gmail.com>
2017-01-20 13:42:52 +0000

    portfolio

--- script/angular.js
+++ script/angular.js
@@ -6,13 +6,14 @@ myApp.controller('myController', function($scope){
 		email: "vimleshsinghk@googlemail.com",
 		mobile: "+44-7720271194",
 		webSite: "http://www.vimlesh.co.uk",
-		birthDate:new Date(1980,04,17),
+		birthDate: new Date(1980,04,17),
 		gender: "male",
 		work: "Web Developer",
 		projects: ["images/ATH.jpg"],
 		avatar: "images/vimlesh.jpg",
-		social: [linkedin={link: "https://www.linkedin.com/in/vimlesh-singh-9b501b137",
-		 icon: "images/linkedin.png"}]
+		social: [linkedin = {link: "https://www.linkedin.com/in/vimlesh-singh-9b501b137",
+		                     icon: "images/linkedin.png"}
+		        ]
 	};
 	$scope.person = person;
 	$scope.mycolor = "rgba(123,15,255,1.0)";
--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
-/* TO STORE THE USER BACKGROUND COLOR */
+
 "use strict";
+if(navigator.userAgent.indexOf("MSIE") != -1 ){
+    document.getElementById("background-control").style.display = none;
+}
+/* TO CHANGE THE USER BACKGROUND COLOR */
 function changeBgColor(){
-    if(sessionStorage.userBgColor){
+    if(sessionStorage.userBgColor!==undefined){
         document.body.style.backgroundColor = sessionStorage.userBgColor;
         document.getElementById("bg-color").value = sessionStorage.userBgColor;
     }
 };
+/* TO STORE THE USER BACKGROUND COLOR */
 function selectedColor(){
 	var userColor = document.getElementById("bg-color").value;
-    if(typeof(Storage) !== "undefined"){
+    document.body.style.backgroundColor = userColor;
+    if(typeof(sessionStorage) !== undefined){
         if(sessionStorage.userBgColor){
             sessionStorage.userBgColor = userColor;
         } else {
             sessionStorage.userBgColor = userColor;
         }
-        document.body.style.backgroundColor = sessionStorage.userBgColor;
     } else {
         alert("Sorry, your browser does not support this...");
     }
